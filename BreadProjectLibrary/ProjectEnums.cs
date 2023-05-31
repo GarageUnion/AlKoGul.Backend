47f7f2c5c0206b17f8771bffffa45657f7ce1960
Bober23 <vanya.popov.2003@gmail.com>
2023-05-31 18:06:12 +0300

    Another fix

---
+++ BreadProjectLibrary/ProjectEnums.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreadProjectLibrary
+{
+    static public class ProjectEnums
+    {
+        public enum BreadCategory
+        {
+            Other,
+            WhiteBread,
+            BlackBread,
+            Pie,
+            Bun,
+            Cake,
+        }
+    }
+}
